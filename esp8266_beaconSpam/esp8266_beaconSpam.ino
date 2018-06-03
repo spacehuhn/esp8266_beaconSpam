abcb9cfa324d28de6a56dd4c222b6eb01bcb2e65
Stefan Kremser <stef.1497@gmail.com>
2018-06-03 18:56:50 +0200

    Version 2

--- esp8266_beaconSpam/esp8266_beaconSpam.ino
+++ esp8266_beaconSpam/esp8266_beaconSpam.ino
@@ -1,183 +1,264 @@
+/*
+  ===========================================
+       Copyright (c) 2018 Stefan Kremser
+              github.com/spacehuhn
+  ===========================================
+*/
+
+// ===== Includes ===== //
 #include <ESP8266WiFi.h>
 
 extern "C" {
-  #include "user_interface.h"
+#include "user_interface.h"
+  typedef void (*freedom_outside_cb_t)(uint8 status);
+  int wifi_register_send_pkt_freedom_cb(freedom_outside_cb_t cb);
+  void wifi_unregister_send_pkt_freedom_cb(void);
+  int wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
 }
-#include <SPI.h>
-#include <SD.h>
+// ==================== //
 
-/* ================ SETTINGS ================ */
-const int maxSize = 1000; // List size
-const int maxLen = 32; // max. SSID length
-const bool spamWPA = true; //spam WPA(2) protected networks
-const bool randomChannel = true; // send on random channels
-const int channel = 11;
-const int chipSelect = D8; // for the SD card reader
-const String fileName = "ssid.txt"; // name of the ssid file on the SD card
-char ssids[maxSize][maxLen] = {"Mom Use This One","Abraham Linksys","Benjamin FrankLAN","Martin Router King","John Wilkes Bluetooth","Pretty Fly for a Wi-Fi","Bill Wi the Science Fi","I Believe Wi Can Fi","Tell My Wi-Fi Love Her","No More Mister Wi-Fi","LAN Solo","The LAN Before Time","Silence of the LANs","House LANister","Winternet Is Coming","Ping’s Landing","The Ping in the North","This LAN Is My LAN","Get Off My LAN","The Promised LAN","The LAN Down Under","FBI Surveillance Van 4","Area 51 Test Site","Drive-By Wi-Fi","Planet Express","Wu Tang LAN","Darude LANstorm","Never Gonna Give You Up","Hide Yo Kids, Hide Yo Wi-Fi","Loading…","Searching…","VIRUS.EXE","Virus-Infected Wi-Fi","Starbucks Wi-Fi","Text ###-#### for Password","Yell ____ for Password","The Password Is 1234","Free Public Wi-Fi","No Free Wi-Fi Here","Get Your Own Damn Wi-Fi","It Hurts When IP","Dora the Internet Explorer","404 Wi-Fi Unavailable","Porque-Fi","Titanic Syncing","Test Wi-Fi Please Ignore","Drop It Like It’s Hotspot","Life in the Fast LAN","The Creep Next Door","Ye Olde Internet"};
-/* ========================================== */
+// ===== Settings ===== //
+const uint8_t channels[] = {1, 6, 11}; // used Wi-Fi channels (1-14)
+const bool wpa2 = false; // WPA2 networks
 
-File ssdiFile;
+/*
+  SSIDs:
+  - don't forget the \n at the end of each SSID!
+  - max. 32 characters per SSID
+  - don't add duplicates! You have to change one character at least
+*/
+const char ssids[] PROGMEM = {
+  "Mom Use This One\n"
+  "Abraham Linksys\n"
+  "Benjamin FrankLAN\n"
+  "Martin Router King\n"
+  "John Wilkes Bluetooth\n"
+  "Pretty Fly for a Wi-Fi\n"
+  "Bill Wi the Science Fi\n"
+  "I Believe Wi Can Fi\n"
+  "Tell My Wi-Fi Love Her\n"
+  "No More Mister Wi-Fi\n"
+  "LAN Solo\n"
+  "The LAN Before Time\n"
+  "Silence of the LANs\n"
+  "House LANister\n"
+  "Winternet Is Coming\n"
+  "Ping’s Landing\n"
+  "The Ping in the North\n"
+  "This LAN Is My LAN\n"
+  "Get Off My LAN\n"
+  "The Promised LAN\n"
+  "The LAN Down Under\n"
+  "FBI Surveillance Van 4\n"
+  "Area 51 Test Site\n"
+  "Drive-By Wi-Fi\n"
+  "Planet Express\n"
+  "Wu Tang LAN\n"
+  "Darude LANstorm\n"
+  "Never Gonna Give You Up\n"
+  "Hide Yo Kids, Hide Yo Wi-Fi\n"
+  "Loading…\n"
+  "Searching…\n"
+  "VIRUS.EXE\n"
+  "Virus-Infected Wi-Fi\n"
+  "Starbucks Wi-Fi\n"
+  "Text ###-#### for Password\n"
+  "Yell ____ for Password\n"
+  "The Password Is 1234\n"
+  "Free Public Wi-Fi\n"
+  "No Free Wi-Fi Here\n"
+  "Get Your Own Damn Wi-Fi\n"
+  "It Hurts When IP\n"
+  "Dora the Internet Explorer\n"
+  "404 Wi-Fi Unavailable\n"
+  "Porque-Fi\n"
+  "Titanic Syncing\n"
+  "Test Wi-Fi Please Ignore\n"
+  "Drop It Like It’s Hotspot\n"
+  "Life in the Fast LAN\n"
+  "The Creep Next Door\n"
+  "Ye Olde Internet\n"
+};
+// ==================== //
 
-char ssidLengths[maxSize];
-char macs[maxSize][6];
-char channels[maxSize]; 
+// run-time variables
+char emptySSID[32];
+char tmpSSID[33];
+uint8_t channelIndex = 0;
+uint8_t macAddr[6];
+uint8_t wifi_channel = 1;
+uint32_t currentTime = 0;
+uint32_t packetSize = 0;
+uint32_t packetCounter = 0;
+uint32_t attackTime = 0;
+uint32_t packetRateTime = 0;
 
-int ssidNum = 50;
-String ssid;
+// beacon frame definition
+uint8_t beaconPacket[109] = {
+  /*  0 - 3  */ 0x80, 0x00, 0x00, 0x00, // Type/Subtype: managment beacon frame
+  /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination: broadcast
+  /* 10 - 15 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
+  /* 16 - 21 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
 
-int _packetSize = 0;
-uint8_t _packet[110];
+  // Fixed parameters
+  /* 22 - 23 */ 0x00, 0x00, // Fragment & sequence number (will be done by the SDK)
+  /* 24 - 31 */ 0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, // Timestamp
+  /* 32 - 33 */ 0x64, 0x00, // Interval: 0x64, 0x00 => every 100ms - 0xe8, 0x03 => every 1s
+  /* 34 - 35 */ 0x31, 0x00, // capabilities Tnformation
 
-unsigned long previousMillis = 0;
+  // Tagged parameters
 
-uint8_t packetBegin[36] = { 
-  0x80, 0x00, 
-  0x00, 0x00, //beacon
-  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, //destination: broadcast
-  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, //source
-  0x01, 0x02, 0x03, 0x04, 0x05, 0x06, //source
-  0xc0, 0x6c, 
-  0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, 
-  0xe8, 0x03, 
-  0x01, 0x04/*, 
-  0x00, 0x06, //SSID size
-  0x72, 0x72, 0x72, 0x72, 0x72, 0x72, //SSID
-  >>beaconPacket_end<<*/
+  // SSID parameters
+  /* 36 - 37 */ 0x00, 0x20, // Tag: Set SSID length, Tag length: 32
+  /* 38 - 69 */ 0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20,
+  0x20, 0x20, 0x20, 0x20, // SSID
+
+  // Supported Rates
+  /* 70 - 71 */ 0x01, 0x08, // Tag: Supported Rates, Tag length: 8
+  /* 72 */ 0x82, // 1(B)
+  /* 73 */ 0x84, // 2(B)
+  /* 74 */ 0x8b, // 5.5(B)
+  /* 75 */ 0x96, // 11(B)
+  /* 76 */ 0x24, // 18
+  /* 77 */ 0x30, // 24
+  /* 78 */ 0x48, // 36
+  /* 79 */ 0x6c, // 54
+
+  // Current Channel
+  /* 80 - 81 */ 0x03, 0x01, // Channel set, length
+  /* 82 */      0x01,       // Current Channel
+
+  // RSN information
+  /*  83 -  84 */ 0x30, 0x18,
+  /*  85 -  86 */ 0x01, 0x00,
+  /*  87 -  90 */ 0x00, 0x0f, 0xac, 0x02,
+  /*  91 -  92 */ 0x02, 0x00,
+  /*  93 - 100 */ 0x00, 0x0f, 0xac, 0x04, 0x00, 0x0f, 0xac, 0x02,
+  /* 101 - 102 */ 0x01, 0x00,
+  /* 103 - 106 */ 0x00, 0x0f, 0xac, 0x02,
+  /* 107 - 108 */ 0x00, 0x00
 };
 
-uint8_t packetEnd[13] = {
-  0x01, 0x08, 0x82, 0x84,
-  0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01,
-  0x03 //channel
-};
+// goes to next channel
+void nextChannel() {
+  uint8_t ch = channels[channelIndex];
+  channelIndex++;
+  if (channelIndex > sizeof(channels)) channelIndex = 0;
 
-uint8_t wpa2tag[26] = {
-  0x30, //RSN tag
-  0x18, //tag length
-  0x01, 0x00, //RSN version
-  0x00, 0x0f, 0xac, 0x02, //cipher (TKIP)
-  0x02, 0x00, //pair cipher
-  0x00, 0x0f, 0xac, 0x04, //cipher (AES)
-  0x00, 0x0f, 0xac, 0x02, //cipher (TKIP)
-  0x01, 0x00, //AKM count
-  0x00, 0x0f, 0xac, 0x02, //PSK
-  0x00, 0x00 //RSN capabilities
-};
-
-//source: https://forum.arduino.cc/index.php?topic=38107.0
-void PrintHex8(uint8_t *data, uint8_t length){
-  Serial.print("0x");
-  for (int i=0; i<length; i++) {
-    if (data[i]<0x10) {Serial.print("0");}
-    Serial.print(data[i],HEX);
-    Serial.print(" ");
+  if (ch != wifi_channel && ch >= 1 && ch <= 14) {
+    wifi_channel = ch;
+    wifi_set_channel(wifi_channel);
   }
 }
 
-void buildPacket(char _ssid[], int len, char _mac[], uint8_t _ch){
-  if(len > 32) len = 32;
-  _packetSize = 0;
-  
-  for(int i=0;i<sizeof(packetBegin);i++) _packet[_packetSize+i] = packetBegin[i];
-  _packetSize += sizeof(packetBegin);
-  
-  _packet[_packetSize] = 0x00;
-  _packet[_packetSize+1] = len;
-  _packetSize += 2;
-
-  for(int i=0;i<len;i++) _packet[_packetSize+i] = _ssid[i];
-  _packetSize += len;
-
-  for(int i=0;i<6;i++) _packet[10+i] = _packet[16+i] = _mac[i];
-  
-  for(int i=0;i<sizeof(packetEnd);i++) _packet[_packetSize+i] = packetEnd[i];
-  _packetSize += sizeof(packetEnd);
-
-  _packet[_packetSize] = _ch;
-
-  if(spamWPA){
-    for(int i=0;i<sizeof(wpa2tag);i++) _packet[_packetSize+i] = wpa2tag[i];
-    _packetSize += sizeof(wpa2tag);
-  }
+// generates random MAC
+void randomMac() {
+  for (int i = 0; i < 6; i++)
+    macAddr[i] = random(256);
 }
 
-bool _send(uint8_t buf[], int len){
-  delay(1);
-  if(wifi_send_pkt_freedom(buf, len, 0) == -1){
-    /* 
-    //for debugging:
-    Serial.print(_packetSize);
-    Serial.print(" : ");
-    PrintHex8(_packet, _packetSize);
-    Serial.println("");
-    */
-    return false;
-  }else return true;
-}
+void setup() {
+  // create empty SSID
+  for (int i = 0; i < 32; i++)
+    emptySSID[i] = ' ';
 
-void setup()
-{
-  Serial.begin(115200);
-  while(!Serial);
-  Serial.println("");
-  Serial.println("");
-
-  wifi_set_opmode(STATION_MODE);
-  wifi_promiscuous_enable(1); 
-
+  // for random generator
   randomSeed(os_random());
+
+  // set packetSize
+  packetSize = sizeof(beaconPacket);
+  if (wpa2) {
+    beaconPacket[34] = 0x31;
+  } else {
+    beaconPacket[34] = 0x21;
+    packetSize -= 26;
+  }
+
+  // generate random mac address
+  randomMac();
+
+  // start serial
+  Serial.begin(115200);
+  Serial.println();
+
+  // get time
+  currentTime = millis();
+
+  // start WiFi
+  WiFi.mode(WIFI_OFF);
+  wifi_set_opmode(STATION_MODE);
+
+  // set channel
+  nextChannel();
+
+  // print out saved SSIDs
+  Serial.println("SSIDs:");
+  int i = 0;
+  int len = sizeof(ssids);
+  while(i < len){
+    Serial.print((char)pgm_read_byte(ssids + i));
+    i++;
+  }
   
-  Serial.print("Initializing SD card...");
-
-  if(SD.begin(chipSelect)) {
-    Serial.println("initialization done.");
-    ssdiFile = SD.open(fileName);
-    if(ssdiFile) {
-      ssidNum = 0;
-      while(ssdiFile.available() != 0 && ssidNum < maxSize) {
-        ssid = ssdiFile.readStringUntil('\n');
-        if(ssid == "") break;
-        ssid.replace("\r","");
-        ssid.toCharArray(ssids[ssidNum],maxLen);
-        ssidNum++;
-        Serial.println(ssid);
-      } 
-      ssdiFile.close();
-    }else Serial.println("error opening ssid.txt");
-  }else{
-    Serial.println("initialization failed!");
-    Serial.println("using default names");
-  }
-
-  for(int i=0;i<ssidNum;i++){
-    ssid = ssids[i];
-    ssidLengths[i] = ssid.length();
-    macs[i][0] = 0x14;
-    macs[i][1] = 0x56;
-    macs[i][2] = 0x8E;
-    for(int h=3;h<6;h++) macs[i][h] = random(256);
-    if(randomChannel) channels[i] = random(1,12);
-    else channels[i] = channel;
-  }
-  Serial.println("starting...");
+  Serial.println();
+  Serial.println("Started \\o/");
+  Serial.println();
 }
 
-void loop(){
+void loop() {
+  currentTime = millis();
 
-  unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis >= 1000){
-    previousMillis = currentMillis;
-    for(int i=0;i<ssidNum;i++){
-      buildPacket(ssids[i], ssidLengths[i], macs[i], channels[i]);
-      wifi_set_channel(channels[i]);
-      if(_send(_packet, _packetSize)){
-        Serial.print(".");
-        delay(1);
-      }
-      else Serial.print(" ");
+  // send out SSIDs
+  if (currentTime - attackTime > 100) {
+    attackTime = currentTime;
+
+    // temp variables
+    int i = 0;
+    int j = 0;
+    int ssidNum = 1;
+    char tmp;
+    int ssidsLen = strlen_P(ssids);
+
+    // go to next channel
+    nextChannel();
+
+    while (i < ssidsLen) {
+      // read out next SSID
+      j = 0;
+      do {
+        tmp = pgm_read_byte(ssids + i + j);
+        j++;
+      } while (tmp != '\n' && j < 32 && i + j < ssidsLen);
+
+      // set MAC address
+      macAddr[5] = ssidNum;
+      ssidNum++;
+
+      memcpy(&beaconPacket[10], macAddr, 6);
+      memcpy(&beaconPacket[16], macAddr, 6);
+      memcpy(&beaconPacket[38], emptySSID, 32);
+      memcpy_P(&beaconPacket[38], &ssids[i], j - 1);
+      beaconPacket[82] = wifi_channel;
+
+      // sent out packet
+      while (wifi_send_pkt_freedom(beaconPacket, packetSize, 0) != 0) delay(1);
+      packetCounter++;
+
+      i += j;
     }
-    Serial.println("");
   }
 
+  // show packet-rate each second
+  if (currentTime - packetRateTime > 1000) {
+    packetRateTime = currentTime;
+    Serial.print("Packets/s: ");
+    Serial.println(packetCounter);
+    packetCounter = 0;
+  }
 }
